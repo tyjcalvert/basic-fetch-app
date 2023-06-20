30f9038b855608aa9c729b214785c0cfd4192e6a
Tyler Calvert <tylercalvert@Tylers-MacBook-Pro.local>
2023-06-20 11:01:56 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
 import { useEffect, useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import './App.css';
 
 function App() {
@@ -7,19 +10,38 @@ function App() {
   const [data, setData] = useState();
 
   const fetchEndpoint = async () => {
+    console.log(process.env.REACT_APP_ENDPOINT)
     const response = await fetch(process.env.REACT_APP_ENDPOINT);
     const parsed = await response.json();
     setData(JSON.stringify(parsed));
   };
 
 
-  useEffect(() =>{
+  useEffect(() => {
     fetchEndpoint();
   }, []);
 
   return (
-    <div className="App">
-      {data}
+    <div style={{ justifyContent: "center", background: "#006699" }}>
+      <Card sx={{ margin: "16px", minWidth: "98vw", background: "#006699", justifyContent: "center", textAlign: "center", }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 22, fontWeight: "bold" }} color="white" gutterBottom>
+            Data can be pulled from internal or external source.
+          </Typography>
+          <TextField 
+          id="tf" 
+          label="Data from endpoint" 
+          variant="outlined"
+          multiline 
+          focused 
+          defaultValue={data} 
+          style={{ width: "90%" }} 
+          InputProps={{
+            readOnly: true,
+          }} 
+        />
+        </CardContent>
+      </Card>
     </div>
   );
 }
